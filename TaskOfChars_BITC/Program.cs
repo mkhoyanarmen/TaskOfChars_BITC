53c2182a5d1289a40addf8eb739f199724b3afc4
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-11 17:20:30 +0400

    Add project files.

---
+++ TaskOfChars_BITC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOfChars_BITC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "absbgbffbogfadksoaitnfacfskfma";
+            string symbols = "";
+            int count = 0, maxCount = 0;
+            char symbMaxCount = '0';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (symbols.Contains(s[i]))
+                    continue;
+                count = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[j] == s[i])
+                        count++;
+                }
+                symbols += s[i];
+                Console.WriteLine($"{s[i]} : {count}");
+                if (maxCount < count)
+                {
+                    symbMaxCount = s[i];
+                    maxCount = count;
+                }
+            }
+            Console.WriteLine("------Max Count------");
+            Console.WriteLine($"{symbMaxCount} : {maxCount}");
+        }
+    }
+}
